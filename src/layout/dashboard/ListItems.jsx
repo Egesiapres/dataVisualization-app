86199609fb70f67a4cfbd56b44e1f3624113e649
Umberto Pasinetti <pasinetti96@gmail.com>
2024-05-25 21:21:32 +0200

    handleDashboard

--- src/layout/dashboard/ListItems.jsx
+++ src/layout/dashboard/ListItems.jsx
@@ -2,13 +2,21 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import { useNavigate } from "react-router";
 
 export default function ListItems() {
+  const navigate = useNavigate()
+
+  const handleDashboard = () => {
+    navigate("/user/dashboard");
+  }
+  
   return (
-    <ListItemButton>
+    <ListItemButton onClick={handleDashboard}>
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
+      
       <ListItemText primary="Dashboard" />
     </ListItemButton>
   );
