b93cbb21b626f379bf3a4bb1e8e14894dbe2f53c
Umberto Pasinetti <pasinetti96@gmail.com>
2024-05-30 11:06:03 +0200

    fix react-route gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/dataVisualization-app/">
+  <HashRouter basename="/">
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
