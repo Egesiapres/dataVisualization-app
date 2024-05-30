8a600dec2730218f32ab47c15873305f654b0060
Umberto Pasinetti <pasinetti96@gmail.com>
2024-05-30 10:27:39 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dataVisualization-app/",
+  base: "https://egesiapres.github.io/dataVisualization-app/",
   plugins: [react()],
 })
